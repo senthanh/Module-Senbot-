3470e14971485b889460287fb9c60a9375983d43
sen thanh <89731735+senthanh@users.noreply.github.com>
2022-04-20 23:33:53 +0700

    Create ghep.js

---
+++ ghep.js
@@ -0,0 +1,48 @@
+module.exports.config = {
+    name: "ghép",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "D-Jukie",//convert Mirai
+    description: "CONSOLE",
+    group: "economy",
+    usages: "",
+    cooldowns: 5
+}
+module.exports.run = async function({ api, event, Users, Currencies }) {
+        const axios = global.nodemodule["axios"];
+        const fs = global.nodemodule["fs-extra"];
+        var data = await Currencies.getData(event.senderID);
+        var money = data.money
+        if( money = 0) api.sendMessage("Con muốn ra ghép đôi à? Thôi tặng ta 1 cái acc clone k dùng đi", event.threadID, event.messageID) //thay số tiền cần trừ vào 0, xóa money = 0
+        else {
+        var tile = Math.floor(Math.random() * 101);
+        
+
+        //let loz = await api.getThreadInfo(event.threadID);
+        var emoji = event.participantIDs;
+        var id = emoji[Math.floor(Math.random() * emoji.length)];
+
+        var namee = (await Users.getData(event.senderID)).name;
+        var name = (await Users.getData(id)).name;
+
+        var arraytag = [];
+        arraytag.push({id: event.senderID, tag: namee});
+        arraytag.push({id: id, tag: name});
+                
+
+        Currencies.setData(event.senderID, options = {money: money - 1000})
+  
+        let Avatar = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
+            fs.writeFileSync( __dirname + "/cache/1.png", Buffer.from(Avatar, "utf-8") );
+        let Avatar2 = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
+            fs.writeFileSync( __dirname + "/cache/2.png", Buffer.from(Avatar2, "utf-8") );
+        var imglove = [];
+              imglove.push(fs.createReadStream(__dirname + "/cache/1.png"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/2.png"));
+        var a = Math.floor(Math.random() * 90) + 10
+        var msg = {body: `Chúc mừng ta đã ghép 2 đứa thành 1 cặp ♥\nTỉ lệ hợp đôi của 2 con là ${a}%\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}
+        return api.sendMessage(msg, event.threadID, event.messageID);
+        //fs.unlinkSync(__dirname + '/cache/1.png');
+        //fs.unlinkSync(__dirname + '/cache/2.png');
+      }
+}
